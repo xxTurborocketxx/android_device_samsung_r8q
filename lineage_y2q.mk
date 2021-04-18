09c82076286bf1eaa2c1d4466fbb850900eb38be
tyro <51808054+tyrohellion@users.noreply.github.com>
2021-04-17 21:37:11 -0400

    Delete lineage_y2q.mk

--- lineage_y2q.mk
+++
@@ -1,8 +0,0 @@
-# Inherit full device configuration
-$(call inherit-product, device/samsung/y2q/full_y2q.mk)
-
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# Product IDs
-PRODUCT_NAME := lineage_y2q
